dae06607dbd24b42090063da42740c79aaa87366
Koloina <rabenjamu@gmail.com>
2023-09-26 15:05:31 +0400

    update assistance

---
+++ .netlify/functions-internal/chunks/abonnement@_@astro.6994a0ec.mjs
@@ -0,0 +1,16 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.cb8f9efd.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./prerender.ebdf2447.mjs').then(n => n.b);
+
+export { page };
