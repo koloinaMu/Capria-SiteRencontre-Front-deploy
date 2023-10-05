e001a4e3caba3ddea5b7edc11dc689d7265a3e57
Koloina <rabenjamu@gmail.com>
2023-10-05 16:41:35 +0400

    comment assistance

---
+++ .netlify/functions-internal/chunks/index@_@astro.b411e63b.mjs
@@ -0,0 +1,16 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.cb8f9efd.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('./prerender.feeef65d.mjs').then(n => n.i);
+
+export { page };
