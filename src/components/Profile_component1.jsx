1272f468ed2559680ab1eee27931d3a46b88656c
Koloina <rabenjamu@gmail.com>
2023-10-09 10:59:20 +0400

    if this.props.visitedId return

--- src/components/Profile_component1.jsx
+++ src/components/Profile_component1.jsx
@@ -115,82 +115,84 @@ export class Profile_component1 extends Component{
         console.log('VISITEDID')
         console.log(this.props.visitedId)
         console.log(this.state)
-        return(
-            <>
-                {this.state.moi==0 &&  <ProfileComponent user={JSON.stringify([this.state.user])} visitedId={this.state.visitedId}  />}
-                
-                <section className="sec-product-detail bg0 p-t-65 p-b-60">
-                    <div className="container">
-                        <div className="p-t-33">
-                            <div className="profile-pic-div bg-dark">
-                                <img src={'/'+this.state.user.photoDeProfil} id="imgPhoto"  />                                
+        if(this.props.visitedId){
+            return(
+                <>
+                    {this.state.moi==0 &&  <ProfileComponent user={JSON.stringify([this.state.user])} visitedId={this.state.visitedId}  />}
+                    
+                    <section className="sec-product-detail bg0 p-t-65 p-b-60">
+                        <div className="container">
+                            <div className="p-t-33">
+                                <div className="profile-pic-div bg-dark">
+                                    <img src={'/'+this.state.user.photoDeProfil} id="imgPhoto"  />                                
+                                </div>
+                                <div className="mt-4">
+                                    <h2 className="text-center">{this.state.user.pseudo}</h2>
+                                    <p className="text-center">{this.state.nbVue} vue(s)</p>
+                                </div>
                             </div>
-                            <div className="mt-4">
-                                <h2 className="text-center">{this.state.user.pseudo}</h2>
-                                <p className="text-center">{this.state.nbVue} vue(s)</p>
-                            </div>
-                        </div>
-
-                        <div className="bor10 mt-4 p-t-43 p-b-40">
-                            <div className="tab01">
-                                <ul className="nav nav-tabs" role="tablist">
-                                    <li className="nav-item p-b-10">
-                                        <a className="nav-link active" data-toggle="tab" href="#about" role="tab">A propos</a>	
-                                    </li>
+    
+                            <div className="bor10 mt-4 p-t-43 p-b-40">
+                                <div className="tab01">
+                                    <ul className="nav nav-tabs" role="tablist">
+                                        <li className="nav-item p-b-10">
+                                            <a className="nav-link active" data-toggle="tab" href="#about" role="tab">A propos</a>	
+                                        </li>
+                                        
+                                        {this.state.moi==1 &&
+                                        <li className="nav-item p-b-10">
+                                            <a className="nav-link" data-toggle="tab" href="#myAccount" role="tab">Mon compte</a>
+                                        </li>
+                                        }
+    
+                                        <li className="nav-item p-b-10">
+                                            <a className="nav-link" data-toggle="tab" href="#photo" role="tab">Photo</a>
+                                        </li>
+    
+                                        {this.state.moi==1 &&
+                                        <li className="nav-item p-b-10">
+                                            <a className="nav-link" data-toggle="tab" href="#vues" role="tab">Vues</a>
+                                        </li>
+                                        }
+                                    </ul>
                                     
-                                    {this.state.moi==1 &&
-                                    <li className="nav-item p-b-10">
-                                        <a className="nav-link" data-toggle="tab" href="#myAccount" role="tab">Mon compte</a>
-                                    </li>
-                                    }
-
-                                    <li className="nav-item p-b-10">
-                                        <a className="nav-link" data-toggle="tab" href="#photo" role="tab">Photo</a>
-                                    </li>
-
-                                    {this.state.moi==1 &&
-                                    <li className="nav-item p-b-10">
-                                        <a className="nav-link" data-toggle="tab" href="#vues" role="tab">Vues</a>
-                                    </li>
-                                    }
-                                </ul>
-                                
-                                <div className="tab-content p-t-43" style={{"display": "block"}}>
-                                    
-                                    <Profile_about_component user={this.state.user} moi={this.state.moi} orientation={this.state.choixOrientation.defaultOpt} />
-                                    
-                                    <div className="modal fade" id="profileEditModal" tabindex="-1" role="dialog" aria-labelledby="profileEditModalTitle" data-backdrop="static" aria-hidden="true">
-                                        <div className="modal-dialog modal-dialog-centered modal-lg" style={{"top": "15%"}} role="document">
-                                            <ProfleEdit user={this.state.user} choixOrientation={this.state.choixOrientation}  />
+                                    <div className="tab-content p-t-43" style={{"display": "block"}}>
+                                        
+                                        <Profile_about_component user={this.state.user} moi={this.state.moi} orientation={this.state.choixOrientation.defaultOpt} />
+                                        
+                                        <div className="modal fade" id="profileEditModal" tabindex="-1" role="dialog" aria-labelledby="profileEditModalTitle" data-backdrop="static" aria-hidden="true">
+                                            <div className="modal-dialog modal-dialog-centered modal-lg" style={{"top": "15%"}} role="document">
+                                                <ProfleEdit user={this.state.user} choixOrientation={this.state.choixOrientation}  />
+                                            </div>
                                         </div>
+    
+                                        {this.state.moi==1&&
+                                        <><Profile_account_component user={this.state.user} />
+                                        <div className="modal fade" id="updatePasswordModal" tabindex="-1" role="dialog" aria-labelledby="updatePasswordModalModalTitle" data-backdrop="static" aria-hidden="true">
+                                            <div className="modal-dialog modal-dialog-centered modal-lg" style={{"top": "15%"}} role="document">
+                                                <UpdatePassword user={this.state.user}  />
+                                            </div>
+                                        </div></>
+                                        }
+                                        <Profile_photo_component moi={this.state.moi} user={this.state.user}  />
+    
+                                        {this.state.moi==1&&
+                                        <Profile_vues_component vues={this.state.vues} />
+                                        }
+                                        
+                                        
+                                        
                                     </div>
-
-                                    {this.state.moi==1&&
-                                    <><Profile_account_component user={this.state.user} />
-                                    <div className="modal fade" id="updatePasswordModal" tabindex="-1" role="dialog" aria-labelledby="updatePasswordModalModalTitle" data-backdrop="static" aria-hidden="true">
-                                        <div className="modal-dialog modal-dialog-centered modal-lg" style={{"top": "15%"}} role="document">
-                                            <UpdatePassword user={this.state.user}  />
-                                        </div>
-                                    </div></>
-                                    }
-                                    <Profile_photo_component moi={this.state.moi} user={this.state.user}  />
-
-                                    {this.state.moi==1&&
-                                    <Profile_vues_component vues={this.state.vues} />
-                                    }
-                                    
-                                    
                                     
                                 </div>
-                                
                             </div>
                         </div>
-                    </div>
-                </section>
-                
-
-            </>
-        )
+                    </section>
+                    
+    
+                </>
+            )   
+        }        
     }
 }
 
