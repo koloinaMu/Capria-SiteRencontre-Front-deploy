d8f2e25ab9834c4250d799bef3a132cb7fe9b298
Koloina <rabenjamu@gmail.com>
2023-10-10 13:23:05 +0400

    re netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 import react from "@astrojs/react";
-import node from '@astrojs/node'
+//import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   //middleware: true,
   output:'hybrid',
-  //adapter: netlify({}),
-  adapter: node({
+  adapter: netlify({}),
+  /*adapter: node({
     mode: "standalone",
-  }),
+  }),*/
   /*experimental: {
     redirects: true
   },*/
