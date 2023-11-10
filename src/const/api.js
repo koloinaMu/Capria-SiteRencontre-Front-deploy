78eba685153b153676558252f0901a96d44d9493
Koloina <rabenjamu@gmail.com>
2023-11-10 11:48:01 +0400

    update api.js

--- src/const/api.js
+++ src/const/api.js
@@ -1,6 +1,7 @@
 function api(query) {
     //return "http://127.0.0.1:3100/"+query
-    return "https://capria-date-back.onrender.com/"+query
+    //return "https://capria-date-back.onrender.com/"+query
+    return "https://site-rencontre.groupe-capria.com/"+query
 }
 
 export default api 
